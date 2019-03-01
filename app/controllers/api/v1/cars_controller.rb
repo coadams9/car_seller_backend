dc24b4218345f0e49eefa4f15979b4f9a707f715
github username <github email address>
2019-03-01 08:54:58 -0500

    car index and show working, rendering json to lh3000

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,7 +1,15 @@
 class Api::V1::CarsController < ApplicationController
 
-  def index
-    cars = Cars.all
-    render json: cars
-  end
+    def index
+        # @cars = Cars.all
+        # render json: cars
+        render json: Car.all
+    end
+
+    def show
+        # @car = Car.find(params[:id])
+        # render json: car.to_json(include: [:sellers])
+        car = Car.find(params[:id])
+        render json: car
+    end
 end
