072ddb273622c9bd638128c02ed901d118df2711
Cory <coadams9@gmail.com>
2019-03-05 16:32:52 -0500

    form finally working

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,14 +1,39 @@
 class Api::V1::CarsController < ApplicationController
     # Never use NEW or EDIT
 
-    def index
-        @cars = Car.all
-        render json: @cars.to_json(include: :sellers)
-    end
+  def index
+    @cars = Car.all
+    render json: @cars.to_json(include: :sellers)
+  end
 
-    def show
-        @car = Car.find(params[:id])
-        render json: @car.to_json(include: :sellers)
+  def show
+    @car = Car.find(params[:id])
+    render json: @car.to_json(include: :sellers)
+  end
+
+  def create
+    @car = Car.new(car_params)
+    @seller = @car.sellers.build(seller_params['sellers'][0])
+    if @car.save
+      render json: { car: @car, seller: @seller }
+    else
+      render json: {
+        car_errors: @car.errors.full_messages,
+        seller_errors: @seller.errors.full_messages
+      }
     end
+  end
+
+  private
+
+  def seller_params
+    params.permit(sellers: [:name, :email, :phoneNum])
+  end
+
+  def car_params
+    params.permit(:make, :modelMake, :year, :color, :image, :description, :price, :favorite)
+  end
+
+
 
 end
--- app/models/seller.rb
+++ app/models/seller.rb
@@ -1,6 +1,5 @@
 class Seller < ApplicationRecord
     has_many :seller_cars
     has_many :cars, through: :seller_cars
-    validates :name, :email, :phoneNum, presence: true
-    validates :name, :email, :phoneNum, uniqueness: true
+    # validates :name, :email, :phoneNum, presence: true, uniqueness: true
 end
